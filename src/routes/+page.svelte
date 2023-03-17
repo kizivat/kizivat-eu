bf857a08afc67637129c3377a2bf43ab06ff173b
David Kizivat <david.kizivat@nettle.ai>
2023-03-17 01:12:31 +0100

    chore: introduce tailwind css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,7 @@
-<h1>David Kizivat</h1>
+<h1 class="text-3xl font-bold underline">David Kizivat</h1>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
