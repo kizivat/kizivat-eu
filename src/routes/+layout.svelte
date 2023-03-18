2b4d1986f97ae9ebf2000a88ff6465bb65c6672b
David Kizivat <david.kizivat@nettle.ai>
2023-03-18 13:30:40 +0100

    feat: add GA4 tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,22 @@
 <script>
 	import '../app.css';
 	import { page } from '$app/stores';
+	import { env } from '$env/dynamic/public';
 </script>
+
+<svelte:head>
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id={env.PUBLIC_GTAG}"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', env.PUBLIC_GTAG);
+	</script>
+</svelte:head>
 
 {#key $page.route}
 	<slot />
