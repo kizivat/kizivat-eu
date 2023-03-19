af858d531f1524527ca35d825e8b96d037f4549b
David Kizivat <david.kizivat@nettle.ai>
2023-03-19 17:48:31 +0100

    chore: extract bio into separate component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,9 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import NettleLink from './NettleLink.svelte';
-
-	let mounted = false;
-	onMount(() => {
-		mounted = true;
-	});
+	import Bio from './Bio.svelte';
 </script>
 
 <main class="h-screen max-w-screen-lg flex flex-col justify-center p-8">
-	<h1 class="text-5xl font-bold mb-8" class:mounted>David Kizivat</h1>
-	<p class="font-serif max-w-prose text-lg" class:mounted>
-		Software engineer at
-		<NettleLink /> and coding educator. I enjoy studying
-		<span class="text-zinc-400">(natural)</span> languages, CS and building stuff.
-	</p>
+	<Bio />
 </main>
 
 <style lang="postcss">
@@ -25,22 +14,5 @@
 
 	:global(body) {
 		@apply flex justify-center;
-	}
-
-	h1:not(.mounted),
-	p:not(.mounted) {
-		@apply opacity-0;
-	}
-
-	h1:not(.mounted) {
-		@apply -translate-y-1/2;
-	}
-
-	p:not(.mounted) {
-		@apply translate-y-1/2;
-	}
-
-	.mounted {
-		@apply opacity-100 translate-y-0 transition-all duration-500;
 	}
 </style>
---
+++ src/routes/Bio.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import NettleLink from './NettleLink.svelte';
+
+	let mounted = false;
+	onMount(() => {
+		mounted = true;
+	});
+</script>
+
+<h1 class="text-5xl font-bold mb-8" class:mounted>David Kizivat</h1>
+<p class="font-serif max-w-prose text-lg" class:mounted>
+	Software engineer at
+	<NettleLink /> and coding educator. I enjoy studying
+	<span class="text-zinc-400">(natural)</span> languages, CS and building stuff.
+</p>
+
+<style lang="postcss">
+	h1:not(.mounted),
+	p:not(.mounted) {
+		@apply opacity-0;
+	}
+
+	h1:not(.mounted) {
+		@apply -translate-y-1/2;
+	}
+
+	p:not(.mounted) {
+		@apply translate-y-1/2;
+	}
+
+	.mounted {
+		@apply opacity-100 translate-y-0 transition-all duration-500;
+	}
+</style>
