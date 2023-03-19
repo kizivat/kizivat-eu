f60dd2fd00a0da1abbcb09835e6fe8d807cc72d4
David Kizivat <david.kizivat@nettle.ai>
2023-03-19 01:47:36 +0100

    feat: add intro animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
-	import { fade } from 'svelte/transition';
+	import { onMount } from 'svelte';
 	import NettleLink from './NettleLink.svelte';
+
+	let mounted = false;
+	onMount(() => {
+		mounted = true;
+	});
 </script>
 
 <main class="h-screen max-w-screen-lg flex flex-col justify-center p-8">
-	<h1 class="text-5xl font-bold mb-8" in:fade>David Kizivat</h1>
-	<p class="font-serif max-w-prose text-lg">
+	<h1 class="text-5xl font-bold mb-8" class:mounted>David Kizivat</h1>
+	<p class="font-serif max-w-prose text-lg" class:mounted>
 		Software engineer at
 		<NettleLink /> and coding educator. I enjoy studying
 		<span class="text-zinc-400">(natural)</span> languages, CS and building stuff.
@@ -20,5 +25,22 @@
 
 	:global(body) {
 		@apply flex justify-center;
+	}
+
+	h1:not(.mounted),
+	p:not(.mounted) {
+		@apply opacity-0;
+	}
+
+	h1:not(.mounted) {
+		@apply -translate-y-1/2;
+	}
+
+	p:not(.mounted) {
+		@apply translate-y-1/2;
+	}
+
+	.mounted {
+		@apply opacity-100 translate-y-0 transition-all duration-500;
 	}
 </style>
